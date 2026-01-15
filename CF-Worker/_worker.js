61a2c3301f62f94ae8251daf49646f38ed97c8c2
New-WeiLai <future20152024@outlook.com>
2026-01-15 17:44:44 +0800

    Update UUID for 我的VL密钥

--- CF-Worker/_worker.js
+++ CF-Worker/_worker.js
@@ -6,7 +6,7 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let 我的VL密钥 = '04c808e2-0b59-47b0-a54b-32fc7ef1c902';//UUID
+let 我的VL密钥 = 'a2fdef2d-e9a3-4612-86d0-0cf443bb4b2f';//UUID
 let 反代IP = 'proxyip.cmliussss.net'; //反代IP
 
 export default {
